4df2e46221dea4a5ce1af4f2695c7e1ba5cdf805
Harsh Yadav <49532763+ashuharsh22@users.noreply.github.com>
2022-06-16 01:24:09 +0530

    travel website

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../App.css";
+import Cards from "../Cards";
+import Herosection from "../Herosection";
+
+function Home() {
+  return (
+    <>
+      <Herosection />
+      <Cards />
+    </>
+  );
+}
+
+export default Home;
